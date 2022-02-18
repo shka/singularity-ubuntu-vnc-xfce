961d22d7ebd022c10398229c8bd3c2330e6347de
Shintaro KATAYAMA <shintaro.katayama@gmail.com>
2022-02-18 21:49:54 +0000

    Support to run in the other Xsession

--- xstartup.sh
+++ xstartup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-unset DBUS_SESSION_BUS_ADDRESS
+unset DBUS_SESSION_BUS_ADDRESS SESSION_MANAGER XDG_CURRENT_DESKTOP XDG_MENU_PREFIX
 [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
 xfce4-session > ~/.vnc/wm.log &
